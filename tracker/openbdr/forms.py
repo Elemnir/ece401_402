13593b3b1c86977a020f595f4e068c9885ee7542
Elemnir <ahoward0920@gmail.com>
2015-03-03 10:32:28 -0500

    Tracker: THP implemented, but untested

---
+++ tracker/openbdr/forms.py
@@ -0,0 +1,42 @@
+from django                 import forms
+from django.http            import Http404
+from django.shortcuts       import get_object_or_404
+from openbdr.models         import Share, Peer
+
+class PeerListRequestForm(forms.Form):
+    info_hash   = forms.CharField(max_length=20)
+    peer_id     = forms.CharField(max_length=20)
+    port        = forms.IntegerField()
+    uploaded    = forms.IntegerField()
+    downloaded  = forms.IntegerField()
+    left        = forms.IntegerField()
+    ip          = forms.CharField(max_length=256, required=False)
+    numwant     = forms.IntegerField(required=False)
+    event       = forms.ChoiceField(required=False,
+        choices=(('started','str'),('stopped','stp'),('completed','cmp'))
+    )
+
+    def clean(self):
+        cd = super(PeerListRequestForm, self).clean()
+        
+        # Get the associated share and its list of authorized peers
+        share = get_object_or_404(Share, info_hash=cd.get('info_hash','')
+        auth_peers = share.peer_list.get_query_set().values('peer_id')
+        
+        # Only allow peers in the authorized list
+        if cd.get('peer_id','') not in auth_peers:
+            raise Http404()
+
+        return cd
+        
+    def process(self):
+        cd = self.cleaned_data
+        share = get_object_or_404(Share, info_hash=cd.get('info_hash','')
+        peer = get_object_or_404(Peer, peer_id=cd.get('peer_id','')
+
+        peer.online = True
+        peer.peer_ip = cd.get('ip','')
+        peer.peer_port = cd['port']
+        peer.save()
+        
+        return share.peer_list.get_query_set()
--- tracker/openbdr/views.py
+++ tracker/openbdr/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts               import render
+from django.http                    import Http404
 from django.contrib.auth.decorators import login_required
+from bencode                        import bencode
 from openbdr.models                 import Account, Peer, Share
-
+from openbdr.forms                  import PeerListRequestForm
 @login_required
 def profile(request):
     """Allow a user or the config tool to manage access permissions"""
@@ -9,4 +11,21 @@ def profile(request):
 
 def tracker(request):
     """Peer communication access point"""
-    pass
+    res = {}
+    try:
+        if request.method == 'GET':
+            plrf = PeerListRequestForm(request.GET)
+            if plrf.is_valid():
+                qs = plrf.process()
+                qs = qs.filter(online__exact=True)
+                res['peers'] = [{'peer id':i.peer_id, 
+                    'ip':i.peer_ip, 
+                    'port':i.peer_port} for i in qs]
+                res['interval'] = 30
+            else:
+                raise Http404()
+    except Http404:
+        res['failure reason'] = 'poorly formed request'
+
+    resb = bencode(res)
+    return HttpResponse(resb, content_type="text/plain")
