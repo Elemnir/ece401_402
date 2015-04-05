fe821307c5289905090154c8994823e8abe601b9
John <john@debian2>
2015-04-05 03:58:53 -0400

    configParse now calculates info_hash for metainfos

--- utility/confClass.cpp
+++ utility/confClass.cpp
@@ -16,6 +16,8 @@
 #include "libtorrent/create_torrent.hpp"
 #include "libtorrent/file_pool.hpp"
 #include "libtorrent/session.hpp"
+#include "libtorrent/lazy_entry.hpp"
+#include "libtorrent/magnet_uri.hpp"
 
 /*Passed to libcurl to write HTTP responses to string*/
 size_t write_to_string(void *ptr, size_t size, size_t count, void * stream);
@@ -23,143 +25,180 @@ size_t write_to_string(void *ptr, size_t size, size_t count, void * stream);
 /*prevent hidden files from being added to torrents*/
 bool file_filter(std::string const& f);
 
+/*Load a torrent into a vector*/
+int load_file(std::string const& filename, std::vector<char>& v, libtorrent::error_code& ec, int limit);
+
 /*Parse the configuration file and load up the confInfo class*/
 /*Probably would have been better form to make this OO and put it in the confInfo class*/
 int confInfo::configParse(ifstream * fin){
 
-		std::string s="", header="";
-		std::ifstream idFin;
-		std::istringstream ss;
+	std::string s="", header="";
+	std::ifstream idFin;
+	std::istringstream ss;
 
-		/*Variable determining if we've passed the daemon & network line*/
-		/*If we have, we're now reading directory info*/
-		int directories = 0;
+	/*Variable determining if we've passed the daemon & network line*/
+	/*If we have, we're now reading directory info*/
+	int directories = 0;
 
-		while(getline(*fin, s)){
-//				printf("Line: %s\n", s.c_str());
+	while(getline(*fin, s)){
+		//printf("Line: %s\n", s.c_str());
 
-				header = "";
+		header = "";
+		ss.clear();
+		ss.str(s);
+		ss >> header;
+
+		if(!directories){
+
+			/*Reading Daemon info*/
+			if(header == "[daemon]"){
+
+				/*getline() followed by ss >> s... is for skipping "PeerIDFile =" and grabbing the value*/
+				/*I should write a 3-line function that does this*/
+				getline(*fin, s);
 				ss.clear();
 				ss.str(s);
-				ss >> header;
+				ss >> s;
+				ss >> s;
+				ss >> PeerIDFile;
 
-				if(!directories){
+				idFin.open(PeerIDFile);
 
-						/*Reading Daemon info*/
-						if(header == "[daemon]"){
-
-								/*getline() followed by ss >> s... is for skipping "PeerIDFile =" and grabbing the value*/
-								/*I should write a 3-line function that does this*/
-								getline(*fin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> s;
-								ss >> s;
-								ss >> PeerIDFile;
-
-								idFin.open(PeerIDFile);
-
-								if(idFin.fail()){
-										fprintf(stderr, "ERROR: Couldn't read PeerID at:%s \nExiting.\n", PeerIDFile.c_str());
-										perror("filemsg:");
-										exit(1);
-								}
-
-								getline(idFin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> peerID;
-
-//								printf("Got the peerID: %s\n", peerID.c_str());
-
-								/*Reading Network info*/
-						}else if(header == "[network]"){
-
-								getline(*fin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> s;
-								ss >> s;
-								ss >> trackerPassword;
-
-								getline(*fin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> s;
-								ss >> s;
-								ss >> trackerUsername;
-
-								getline(*fin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> s;
-								ss >> s;
-								ss >> trackerDomain;
-
-								getline(*fin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> s;
-								ss >> s;
-
-								/*TO-DO:Need to check here to see if it's auto*/
-								ss >> DaemonPort;
-
-								//              printf("Reached the network header\n");
-								directories = 1;
-						}
-				}else{
-						/*Reading a directory from config*/
-						/*Clearly I should be performing a better check here to see if it's directory*/
-						if(header[0] == '['){
-								DirectoryInfo * newDI = new DirectoryInfo;
-
-//								printf("Reading a directory\n");
-								getline(*fin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> s;
-								ss >> s;
-								ss >> newDI->directoryPath;
-
-								getline(*fin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> s;
-								ss >> s;
-								ss >> newDI->torrentPath;
-
-								getline(*fin, s);
-								ss.clear();
-								ss.str(s);
-								ss >> s;
-								ss >> s;
-								ss >> newDI->scanRate;
-
-								getline(*fin, s);
-
-								ss.clear();
-								ss.str(s);
-
-								ss >> s;
-								ss >> s;
-								ss >> newDI->share_id;
-
-								//              if(existingFile(DI->torrentPath)){
-								//                  create_info_hash(DI);
-								//                  printf("\n\nHash: ");
-								//                  for(int i=0; i<20; i++){
-								//                      printf("%c ", (char)(DI->info_hash[i]));
-								//                  }
-								//                  printf("\n\n");
-								//              }
-								DI.insert(make_pair(newDI->directoryPath, newDI));
-						}
+				if(idFin.fail()){
+					fprintf(stderr, "ERROR: Couldn't read PeerID at:%s \nExiting.\n", PeerIDFile.c_str());
+					perror("filemsg:");
+					exit(1);
 				}
 
+				getline(idFin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> peerID;
+
+				//printf("Got the peerID: %s\n", peerID.c_str());
+
+				/*Reading Network info*/
+			}else if(header == "[network]"){
+
+				getline(*fin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> s;
+				ss >> s;
+				ss >> trackerPassword;
+
+				getline(*fin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> s;
+				ss >> s;
+				ss >> trackerUsername;
+
+				getline(*fin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> s;
+				ss >> s;
+				ss >> trackerDomain;
+
+				getline(*fin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> s;
+				ss >> s;
+
+				/*TO-DO:Need to check here to see if it's auto*/
+				ss >> DaemonPort;
+
+				//printf("Reached the network header\n");
+				directories = 1;
+			}
+		}else{
+			/*Reading a directory from config*/
+			/*Clearly I should be performing a better check here to see if it's directory*/
+			if(header[0] == '['){
+				DirectoryInfo * newDI = new DirectoryInfo;
+
+				//printf("Reading a directory\n");
+				getline(*fin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> s;
+				ss >> s;
+				ss >> newDI->directoryPath;
+
+				getline(*fin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> s;
+				ss >> s;
+				ss >> newDI->torrentPath;
+
+				getline(*fin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> s;
+				ss >> s;
+				ss >> newDI->scanRate;
+
+				getline(*fin, s);
+				ss.clear();
+				ss.str(s);
+				ss >> s;
+				ss >> s;
+				ss >> newDI->share_id;
+
+				int item_limit = 1000000;
+				int depth_limit = 1000;
+
+				std::vector<char> buf;
+				libtorrent::error_code ec;
+
+				int ret = load_file(newDI->torrentPath.c_str(), buf, ec, 40 * 1000000);
+
+				if(ret == -1){
+					fprintf(stderr, "file too big, aborting\n");
+					return 1;
+				}
+
+				if(ret != 0){
+					fprintf(stderr, "failed to load file: %s\n", ec.message().c_str());
+					return 1;
+				}
+
+				libtorrent::lazy_entry e;
+				int pos = -1;
+//				printf("decoding. recursion limit: %d total item count limit: %d\n", depth_limit, item_limit);
+				ret = libtorrent::lazy_bdecode(&buf[0], &buf[0] + buf.size(), e, ec, &pos, depth_limit, item_limit);
+
+				if(ret != 0){
+					fprintf(stderr, "failed to decode: '%s' at character: %d\n", ec.message().c_str(), pos);
+					return 1;
+				}
+
+				libtorrent::torrent_info t(e, ec);
+				
+				if(ec){
+					fprintf(stderr, "%s\n", ec.message().c_str());
+					return 1;
+				}
+
+				e.clear();
+				std::vector<char>().swap(buf);
+
+				char ih[41];
+				libtorrent::to_hex((char const*)&t.info_hash()[0], 20, ih);
+				
+//				printf("info hash strlen: %d info_hash: %s\n", strlen(ih), ih);
+//				printf("strlen: %d to_hex: %s\n", strlen(t.info_hash().to_string().c_str()), libtorrent::to_hex(t.info_hash().to_string()).c_str());
+
+				DI.insert(make_pair(newDI->directoryPath, newDI));
+			}
 		}
 
-		return 0;
+	}
+
+	return 0;
 }
 
 /*Pings the read_share tracker url to get a share*/
@@ -167,78 +206,78 @@ int confInfo::configParse(ifstream * fin){
 /*Still works*/
 string confInfo::read_share(DirectoryInfo *DI){
 
-		/*Pings the read_share url to get a share*/
-		/*Need to change to send w/ info_hash*/
-		/*Still works*/
+	/*Pings the read_share url to get a share*/
+	/*Need to change to send w/ info_hash*/
+	/*Still works*/
 
-		string response = "";
-		string s = "";
-		s+=trackerDomain;
-		s+="read_share/?peer_id=";
-		s+=peerID;
-		s+="&share_id=";
-		s+=DI->share_id;
-		//      s+="&info_hash=";
-		//      s.append((char *)(DI->info_hash));
+	string response = "";
+	string s = "";
+	s+=trackerDomain;
+	s+="read_share/?peer_id=";
+	s+=peerID;
+	s+="&share_id=";
+	s+=DI->share_id;
+	//      s+="&info_hash=";
+	//      s.append((char *)(DI->info_hash));
 
-		printf("\nhere is the formatted read_share request string: %s\n", s.c_str());
-		CURL *curl;
-		CURLcode res;
-		curl = curl_easy_init();
+	printf("\nread_share request string: %s\n", s.c_str());
+	CURL *curl;
+	CURLcode res;
+	curl = curl_easy_init();
 
-		if(curl){
-				curl_easy_setopt(curl, CURLOPT_URL, s.c_str());
-				curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-				curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
-				curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+	if(curl){
+		curl_easy_setopt(curl, CURLOPT_URL, s.c_str());
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-				res = curl_easy_perform(curl);
+		res = curl_easy_perform(curl);
 
-				if(res != CURLE_OK){
-						fprintf(stderr, "curl_easy_perform() GET failed: %s\n", curl_easy_strerror(res));
-				}
-
-				printf("The response: %s\n", response.c_str());
-				curl_easy_cleanup(curl);
+		if(res != CURLE_OK){
+			fprintf(stderr, "curl_easy_perform() GET failed: %s\n", curl_easy_strerror(res));
 		}
 
-		return response;
+		printf("\nThe response: %s\n\n", response.c_str());
+		curl_easy_cleanup(curl);
+	}
+
+	return response;
 }
 
 /*Update a share on the tracker*/
 string confInfo::update_share(DirectoryInfo *DI){
 
-		CURL *curl;
-		CURLcode res;
-		string response = "";
-		curl = curl_easy_init();
+	CURL *curl;
+	CURLcode res;
+	string response = "";
+	curl = curl_easy_init();
 
-		if(curl){
-				struct curl_httppost *post=NULL;
-				struct curl_httppost *last=NULL;
-				curl_formadd(&post, &last, CURLFORM_COPYNAME, "share_id", CURLFORM_COPYCONTENTS, DI->share_id.c_str(), CURLFORM_END);
-				//curl_formadd(&post, &last, CURLFORM_COPYNAME, "info_hash", CURLFORM_COPYCONTENTS, DI->info_hash, CURLFORM_END);
-				curl_formadd(&post, &last, CURLFORM_COPYNAME, "share_file", CURLFORM_FILECONTENT, DI->torrentPath.c_str(),CURLFORM_END);
-				curl_easy_setopt(curl, CURLOPT_HTTPPOST, post);
-				curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
-				curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+	if(curl){
+		struct curl_httppost *post=NULL;
+		struct curl_httppost *last=NULL;
+		curl_formadd(&post, &last, CURLFORM_COPYNAME, "share_id", CURLFORM_COPYCONTENTS, DI->share_id.c_str(), CURLFORM_END);
+		//curl_formadd(&post, &last, CURLFORM_COPYNAME, "info_hash", CURLFORM_COPYCONTENTS, DI->info_hash, CURLFORM_END);
+		curl_formadd(&post, &last, CURLFORM_COPYNAME, "share_file", CURLFORM_FILECONTENT, DI->torrentPath.c_str(),CURLFORM_END);
+		curl_easy_setopt(curl, CURLOPT_HTTPPOST, post);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-				res = curl_easy_perform(curl);
+		res = curl_easy_perform(curl);
 
-				if(res != CURLE_OK){
-						fprintf(stderr, "curl_easy_perform() POST failed: %s\n", curl_easy_strerror(res));
-				}
-
-				curl_formfree(post);
+		if(res != CURLE_OK){
+			fprintf(stderr, "curl_easy_perform() POST failed: %s\n", curl_easy_strerror(res));
 		}
 
-		return response;
+		curl_formfree(post);
+	}
+
+	return response;
 }
 
 /*Passed to libcurl to write HTTP responses to string */
 size_t write_to_string(void *ptr, size_t size, size_t count, void * stream){
-		((string *)stream)->append((char*)ptr, 0, size*count);
-		return size*count;
+	((string *)stream)->append((char*)ptr, 0, size*count);
+	return size*count;
 }
 
 int confInfo::torCreate(DirectoryInfo * DI){
@@ -258,8 +297,8 @@ int confInfo::torCreate(DirectoryInfo * DI){
 	}
 
 	libtorrent::create_torrent t(fs);
-	t.add_tracker("http://tempTracker.com/");
-	t.set_creator("creatorExample");
+	t.add_tracker(trackerDomain.c_str());
+	t.set_creator(trackerUsername.c_str());
 
 	/*set_piece_hashes requires the path of the directory containing the monitored directory..*/
 	/*Need to remove the directory name...*/
@@ -286,32 +325,95 @@ int confInfo::torCreate(DirectoryInfo * DI){
 /*Need to finish this; stopped because info_hash isn't set*/
 int confInfo::download_torrent(libtorrent::session *s, DirectoryInfo * DI){
 
-		libtorrent::error_code ec;
+	libtorrent::error_code ec;
 
-		libtorrent::add_torrent_params p;
-		p.save_path = DI->directoryPath;
-		p.ti = new libtorrent::torrent_info(DI->torrentPath, ec);
-		if(ec){
-				fprintf(stderr, "%s\n", ec.message().c_str());
-				return -1;
-		}
+	libtorrent::add_torrent_params p;
+	p.save_path = DI->directoryPath;
+	p.ti = new libtorrent::torrent_info(DI->torrentPath, ec);
+	if(ec){
+		fprintf(stderr, "%s\n", ec.message().c_str());
+		return -1;
+	}
 
-		//char ih[41];
-		//libtorrent::torrent_info t = p.ti;
+	//char ih[41];
+	//libtorrent::torrent_info t = p.ti;
 
-		//to_hex((char const*)&ti.info_hash()[0], 20, ih);
-		//printf("info_hash: %s\n", ih);
+	//to_hex((char const*)&ti.info_hash()[0], 20, ih);
+	//printf("info_hash: %s\n", ih);
 
-		return 0;
+	return 0;
 }
-
 
 /*Ripped from libtorrent examples*/
 /*Prevent hidden files from being added*/
 bool file_filter(std::string const& f){
 
-		if(libtorrent::filename(f)[0] == '.') return false;
-		fprintf(stderr, "%s\n", f.c_str());
-		return true;
+	if(libtorrent::filename(f)[0] == '.') return false;
+	fprintf(stderr, "%s\n", f.c_str());
+	return true;
 }
+
+/*this loads a torrent file into a passed in vector*/
+int load_file(std::string const& filename, std::vector<char>& v, libtorrent::error_code& ec, int limit = 8000000)
+{
+	printf("Reached load with: %s\n", filename.c_str());
+	ec.clear();
+	FILE* f = fopen(filename.c_str(), "rb");
+	if (f == NULL)
+	{
+		ec.assign(errno, boost::system::get_generic_category());
+		return -1;
+	}
+
+	int r = fseek(f, 0, SEEK_END);
+	if (r != 0)
+	{
+		ec.assign(errno, boost::system::get_generic_category());
+		fclose(f);
+		return -1;
+	}
+	long s = ftell(f);
+	if (s < 0)
+	{
+		ec.assign(errno, boost::system::get_generic_category());
+		fclose(f);
+		return -1;
+	}
+
+	if (s > limit)
+	{
+		fclose(f);
+		return -2;
+	}
+
+	r = fseek(f, 0, SEEK_SET);
+	if (r != 0)
+	{
+		ec.assign(errno, boost::system::get_generic_category());
+		fclose(f);
+		return -1;
+	}
+
+	v.resize(s);
+	if (s == 0)
+	{
+		fclose(f);
+		return 0;
+	}
+
+	r = fread(&v[0], 1, v.size(), f);
+	if (r < 0)
+	{
+		ec.assign(errno, boost::system::get_generic_category());
+		fclose(f);
+		return -1;
+	}
+
+	fclose(f);
+
+	if (r != s) return -3;
+
+	return 0;
+}
+
 
--- utility/daemon.cpp
+++ utility/daemon.cpp
@@ -1,5 +1,5 @@
 /*********************************************************************/
-/*This is the client. It will perform the following tasks:*/
+/*This is the client. It will perform the following tasks:           */
 /*********************************************************************/
 /*  -Daemon process                                                  */
 /*  -Monitors directories for changes via iNotify                    */
@@ -12,11 +12,10 @@
 /*  -Filetransfer between peers will utilize BitTorrent Protocol     */
 /*********************************************************************/
 //TO-DO List:
-//1): Calculate info_hash
-//2): update read_share function
-//3): finish update_share
-//4): put read_shares and update_shares in the correct places
-//5): Set update timer according to config tool
+//1): update read_share function
+//2): finish update_share
+//3): put read_shares and update_shares in the correct places
+//4): Set update timer according to config tool
 
 #include <string>
 #include <vector>
@@ -57,9 +56,6 @@ void * read_share_timer(void * CI);
 /*cntl+c signal handler*/
 void cntl_c_handler(int dummy);
 
-/*Creates an info hash given a directory*/
-//void create_info_hash(DirectoryInfo * DI);
-
 /*read_share thread*/
 pthread_t tcb;
 void * status;
@@ -74,9 +70,9 @@ int main(int argc, const char* argv[])
 
 	/*set cntl-c sig handler*/
 	signal(SIGINT, cntl_c_handler);
-	
+
 	/*Directory Monitoring*/
-    Inotify notify;
+	Inotify notify;
 
 	/*libtorrent variables*/
 	libtorrent::session sess;
@@ -88,12 +84,12 @@ int main(int argc, const char* argv[])
 	std::map<string, DirectoryInfo *>::iterator mit;
 
 	/*General Variables*/
-    std::vector<fs::path> watched_directories;
-	
+	std::vector<fs::path> watched_directories;
+
 	/*Kept a string version of the vector because I was using it for stuff*/
 	std::vector<std::string> watched_directories2;
-    int timer_fd, bytes_read; //update_interval = 1;
-    char buffer[1024];
+	int timer_fd, bytes_read; //update_interval = 1;
+	char buffer[1024];
 
 	/*Open Configuration File for parsing*/
 	std::ifstream * fin = new std::ifstream;
@@ -122,14 +118,14 @@ int main(int argc, const char* argv[])
 		fprintf(stderr, "ERROR: Config file specifies no directories...\n");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	/*Loop through directories found in config file...*/
 	/*Check for existing metainfo: if it does, does it need updates?*/
 	/*If it doesn't exist, create it!*/
 	for(mit=CI->DI.begin(); mit!=CI->DI.end(); mit++){
 
 		printf("\nMonitoring: %s\n", mit->second->directoryPath.c_str());	
-		
+
 		/*Maurice's Code*/
 		fs::path directory(mit->second->directoryPath.c_str());
 		fs::path canonical_directory = boost::filesystem::canonical(directory);
@@ -144,24 +140,24 @@ int main(int argc, const char* argv[])
 		if(!existingFile(mit->second->torrentPath)){
 
 			/*I should hit read_share here & see if torrent exists already*/
-			
+
 			if(!(CI->torCreate(mit->second))){
 				fprintf(stderr, "ERROR: unable to create metainfo.. continuing\n");
 			}
 
 		}else{
-		/*Torrent file exists!*/
-	
+			/*Torrent file exists!*/
+
 			printf("\nTorrent file already exists! Reading share for updates...\n");
-			
+
 			string response = CI->read_share(mit->second);
-			printf("Read share was succesful! (Share not written here yet though)\n");
+			printf("\nRead share was successful! (Share not written here yet though)\n");
 		}
 	}
 
 	/*libtorrent; open session to communicate w/ peers*/
 	sess.listen_on(std::make_pair(6881, 6889), ec);
-	
+
 	if(ec){
 		fprintf(stderr, "failed to open listen socket: %s\n", ec.message().c_str());
 		exit(EXIT_FAILURE);
@@ -169,41 +165,41 @@ int main(int argc, const char* argv[])
 
 	/*Not calling stop_upnp() anywhere or manually port mapping;*/
 	sess.start_upnp();
-	
+
 	if(pthread_create(&tcb, NULL, read_share_timer, CI) != 0){
 		perror("read_share_timer");
 		exit(1);
 	}
 
 	// Create timer
-    timer_fd = create_timer(60);
+	timer_fd = create_timer(60);
 	//timer_fd = create_timer(update_interval);
 
-    while (1) {
-        
+	while (1) {
+
 		printf("\nModification checker loop tick\n");
 		fflush(stdout);
 
 		bytes_read = read(timer_fd, &buffer, 1024);
-        if (bytes_read == -1) {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
+		if (bytes_read == -1) {
+			perror("read");
+			exit(EXIT_FAILURE);
+		}
 
-        // Read the events in the event-queue, updating the directory tree in the process
-        notify.ReadEvents();
+		// Read the events in the event-queue, updating the directory tree in the process
+		notify.ReadEvents();
 
-        // Check status of each watched directory
-        for(unsigned int i=0; i<watched_directories.size(); i++) {
-            
+		// Check status of each watched directory
+		for(unsigned int i=0; i<watched_directories.size(); i++) {
+
 			fs::path w_directory = watched_directories[i];
 			std::string directory = watched_directories2[i];
-            enum UPDATE_FLAG flag = notify.GetUpdateFlag(w_directory);
+			enum UPDATE_FLAG flag = notify.GetUpdateFlag(w_directory);
 
-            if (flag == MODIFIED) {
-                
+			if (flag == MODIFIED) {
+
 				// update torrent in here! (create updated metainfo & ping tracker w/ it)
-                printf("    %s: MODIFIED\n", directory.c_str());
+				printf("    %s: MODIFIED\n", directory.c_str());
 
 				/*First, hit read_share for update if there is one. Else, torCreate and update_share*/
 				if(!(CI->torCreate(CI->DI[directory]))){
@@ -212,67 +208,67 @@ int main(int argc, const char* argv[])
 					printf("Succesfully updated: %s\n", w_directory.c_str());
 				}
 			}
-            else if (flag == DOESNOTEXIST) {
-                // do something
-					printf("DOESNOTEXIST\n");
-            }
-        }
-    }
-    
-    if (close(timer_fd) == -1) {
-        perror("close");
-        exit(EXIT_FAILURE);
-    }
+			else if (flag == DOESNOTEXIST) {
+				// do something
+				printf("DOESNOTEXIST\n");
+			}
+		}
+	}
+
+	if (close(timer_fd) == -1) {
+		perror("close");
+		exit(EXIT_FAILURE);
+	}
 }
 
 /*maintains synchronized update-check time*/
 int create_timer(int interval)
 {
-		int timer_fd_;
-		struct itimerspec time;
+	int timer_fd_;
+	struct itimerspec time;
 
-		// Set interval
-		time.it_value.tv_sec = interval;
-		time.it_interval.tv_sec = interval;
-		time.it_value.tv_nsec = 0;
-		time.it_interval.tv_nsec = 0;
+	// Set interval
+	time.it_value.tv_sec = interval;
+	time.it_interval.tv_sec = interval;
+	time.it_value.tv_nsec = 0;
+	time.it_interval.tv_nsec = 0;
 
-		timer_fd_ = timerfd_create(CLOCK_MONOTONIC, 0);
-		if (timer_fd_ == -1) {
-				perror("timerfd_create");
-				exit(EXIT_FAILURE);
-		}
+	timer_fd_ = timerfd_create(CLOCK_MONOTONIC, 0);
+	if (timer_fd_ == -1) {
+		perror("timerfd_create");
+		exit(EXIT_FAILURE);
+	}
 
-		if (timerfd_settime(timer_fd_, 0, &time, NULL) == -1) {
-				perror("timerfd_settime");
-				exit(EXIT_FAILURE);
-		}
+	if (timerfd_settime(timer_fd_, 0, &time, NULL) == -1) {
+		perror("timerfd_settime");
+		exit(EXIT_FAILURE);
+	}
 
-		return timer_fd_;
+	return timer_fd_;
 }
 
 /*Does the file exist?*/
 bool existingFile (const std::string & name){
 
-		struct stat buffer;
-		return (stat (name.c_str(), &buffer) == 0);
+	struct stat buffer;
+	return (stat (name.c_str(), &buffer) == 0);
 }
 
 /*Thread function checking for directory meta-info updates on an interval*/
 /*Currently set to 60 seconds*/
 void * read_share_timer(void * CI){
-	
+
 	std::ofstream fout;
 	std::map<string, DirectoryInfo *>::iterator mit;
-	
+
 	confInfo * ci = (confInfo *)CI;
-	
+
 	while(1){
-		
+
 		sleep(60);
-		
+
 		for(mit = ci->DI.begin(); mit != ci->DI.end(); mit++){
-	
+
 			string response = ci->read_share(mit->second);
 			//fout.open(mit->second->torrentPath);
 			//fout << response;
@@ -281,7 +277,7 @@ void * read_share_timer(void * CI){
 
 		/*stopLoop is set in the cntl+c signal handler*/
 		if(stopLoop)break;
-		
+
 	}
 
 	return NULL;
@@ -291,7 +287,7 @@ void * read_share_timer(void * CI){
 void cntl_c_handler(int dummy){
 
 	signal(SIGINT, cntl_c_handler);
-	printf("\nYou just typed cntl-c\n");
+	printf("\nShutting down. Waiting for next read_share_timer tick to end process...\n");
 	stopLoop = 1;
 	if(pthread_join(tcb, &status) != 0){
 		perror("pthread_join");
@@ -299,12 +295,3 @@ void cntl_c_handler(int dummy){
 	}
 	exit(1);
 }
-
-/*Creates an info_hash given a directory*/
-//void create_info_hash(DirectoryInfo * DI){
-
-//		std::ifstream fin(DI->torrentPath);
-//		std::string s((std::istreambuf_iterator<char>(fin)), std::istreambuf_iterator<char>());
-//		SHA1((unsigned char *)s.c_str(), strlen(s.c_str()), DI->info_hash);
-		
-//}
